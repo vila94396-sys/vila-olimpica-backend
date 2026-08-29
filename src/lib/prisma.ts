5fbf01b37d7ffd55c7c163e1471517cc24a51ce4
vila94396-sys <vila94396@gmail.com>
2026-08-29 14:46:32 +0200

    Fix Prisma v7: use @prisma/adapter-pg driver adapter for PostgreSQL

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const prisma = new PrismaClient({ adapter });
 
